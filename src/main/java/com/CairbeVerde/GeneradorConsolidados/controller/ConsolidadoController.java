6183b1b5fa8a50b0ac1507e448fad45880466b69
maxiio178 <maxiiomartelo@gmail.com>
2023-06-11 17:56:58 -0500

    Inizialicando prueba Spring (prueba aprovada)

--- src/main/java/com/CairbeVerde/GeneradorConsolidados/controller/ConsolidadoController.java
+++ src/main/java/com/CairbeVerde/GeneradorConsolidados/controller/ConsolidadoController.java
@@ -1,4 +1,14 @@
 package com.CairbeVerde.GeneradorConsolidados.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 //Se encarga de mostrar y los archivos xls atravez de consultas http segun la empresa
+@RestController
 public class ConsolidadoController {
+    @GetMapping("/chek")
+    public String test() {
+        return "OK";
+    }
 }
+
--- src/main/java/com/CairbeVerde/GeneradorConsolidados/service/ListarService.java
+++ src/main/java/com/CairbeVerde/GeneradorConsolidados/service/ListarService.java
@@ -1,4 +1,13 @@
 package com.CairbeVerde.GeneradorConsolidados.service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 // Clase encargada de generar lista de consolidado segun la empresa que lo necesite
+@RestController
 public class ListarService {
+    @GetMapping("/chek")
+    public String test() {
+        return "OK";
+    }
 }
